5d1f442949512504ad26047ff21bc4697fa80db4
jhonalexander1606 <jhonalexandercalderonmunoz@gmail.com>
2023-05-07 12:40:57 -0500

    se agrego el codigo

--- estudiante.py
+++ estudiante.py
@@ -10,7 +10,7 @@ ventana_principal = tk.Tk()
 ventana_principal.title("regisgtro")
 
 # tamaño de la ventana
-ventana_principal.geometry("600x500")
+ventana_principal.geometry("600x600")
 
 # deshabilitar boton de maximizar
 ventana_principal.resizable(False, False)
@@ -29,27 +29,26 @@ ventana_principal.iconphoto(True, icon)
 #imagen de fondo ventana principal
 f_vp = PhotoImage(file="img/fondo_ven_p.png")
 
-# label fondo de la ventana principal
+# fondo de la ventana principal
 
 fondo_ventana_prin = Label(ventana_principal, image = f_vp) 
 fondo_ventana_prin.place(x= 0, y=0)
 
 #titulo de la ventana principal
 
-titulo = Label(fondo_ventana_prin, text="Registro estudiante")
-titulo.config(bg = "lemon chiffon",fg="black", font=("Helvetica", 20))
-titulo.place(x=180,y=10)
-
+titulo_n = Label(fondo_ventana_prin, text="Registro academico")
+titulo_n.config(bg = "lavender blush",fg="black", font=("Helvetica", 20))
+titulo_n.place(x=50,y=10)
 
 # imagen para el label de notas
 
-fondo_fr_notas = PhotoImage(file="img/fondo_frame_notas.png" )
+fondo_lb_notas = PhotoImage(file="img/fondo_frame_notas.png" )
 
 #--------------------------------
-# label entrada datos notas
+# label entrada datos
 #--------------------------------
 
-label_entrada = Label(ventana_principal, image =fondo_fr_notas )
+label_entrada = Label(ventana_principal, image =fondo_lb_notas )
 label_entrada.config(bg="white", width=575, height=215)
 label_entrada.place(x=10, y=60)
 
@@ -59,9 +58,16 @@ label_entrada.place(x=10, y=60)
 nota_i= PhotoImage(file = "img/notas.png") 
 label_logo = Label(label_entrada, image = nota_i)
 label_logo.place(x= 30, y = 15)
-
-
 # imagenes de fondo
+
+# titulo del registro salud
+titulo_n = Label(fondo_ventana_prin, text="Registro medico")
+titulo_n.config(bg = "lavender blush",fg="black", font=("Helvetica", 20))
+titulo_n.place(x=50,y=290)
+
+# imagen para el label de la salud
+
+
 
 
 
